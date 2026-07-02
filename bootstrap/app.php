67f94d08aa2f2bcc1709823893070b61daa3699d
Severino Mongo <mongosr@halotel.co.tz>
2026-07-02 16:43:46 +0300

    Infrastructure: registered the admin middleware class alias to resolve route resolution 500 crashes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,12 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        
-        // 👈 THIS IS WHAT WE ARE ADDING: Registering your middleware nickname
+        // 🔐 Register your custom 'admin' alias here!
         $middleware->alias([
-            'admin' => \App\Http\Middleware::class,
+            'admin' => \App\Http\Middleware\Admin::class,
         ]);
-
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
