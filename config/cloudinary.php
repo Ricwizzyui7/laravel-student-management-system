d96f9b1519b70bf39b1a4647361149fb96d64ee0
Severino Mongo <mongosr@halotel.co.tz>
2026-07-06 09:15:01 +0300

    Infrastructure: removed boot dynamic publishing and committed native cloudinary configurations mapping

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'cloud_url' => env('CLOUDINARY_URL=cloudinary://<your_api_key>:<your_api_secret>@vrhj5skk'),
+    'cloud_url' => env('CLOUDINARY_URL=cloudinary://<237295274452647>:<6bhZ8oxrva04-x0_rDymNcnQ3pw>@vrhj5skk'),
     
     'cloud_name' => env('vrhj5skk'),
     'api_key'    => env('237295274452647'),
