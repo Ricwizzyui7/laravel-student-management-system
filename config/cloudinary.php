642ad9a77feaa6fcbf3a52cf816af484bc155899
Severino Mongo <mongosr@halotel.co.tz>
2026-07-06 09:17:54 +0300

    Fix: corrected env helper syntax in cloudinary config file

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'cloud_url' => env('CLOUDINARY_URL=cloudinary://<237295274452647>:<6bhZ8oxrva04-x0_rDymNcnQ3pw>@vrhj5skk'),
-    
-    'cloud_name' => env('vrhj5skk'),
-    'api_key'    => env('237295274452647'),
-    'api_secret' => env('6bhZ8oxrva04-x0_rDymNcnQ3pw')
+    'cloud_url'   => env('CLOUDINARY_URL'),
+
+    'cloud_name'  => env('CLOUDINARY_CLOUD_NAME'),
+    'api_key'     => env('CLOUDINARY_API_KEY'),
+    'api_secret'  => env('CLOUDINARY_API_SECRET'),
 ];
